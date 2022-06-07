3a422231c9e58057555847b9c200e8ed8a8205c5
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-06-06 17:11:46 -0700

    Light & Dark Mode setup
    - Established a color theme switcher
    - Some initial styling to test the theme switcher
    - Converted App.tsx from a class component to a function component that uses state hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>TODO</h1>
-        </header>
-      </div>
-    )
+const App = () => {
+  // state variables
+  // mode for establish light mode or dark mode
+  let [theme, setTheme] = useState('dark')
+
+  // images for mode switch
+  const sunIcon = './images/icon-sun.svg';
+  const moonIcon = './images/icon-moon.svg';
+  let icon: string;
+  if(theme === 'light'){icon = sunIcon}
+  else{icon = moonIcon};
+
+  const iconClick = () => {
+    if(theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
   }
+
+  return(
+    <div className='app' data-theme={theme}>
+    <main>
+      <header>
+        <h1 className='title'>TODO</h1>
+        <img src={icon} alt='color theme switch' onClick={iconClick} className='icon' />
+      </header>
+    </main>
+    <footer>
+
+    </footer>
+    </div>
+  )
 }
 
 export default App;
