838c734e5510ad06a3472cff770f18b91f7a5f26
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-06-11 19:14:27 -0700

    Added hover states and alt text for remove button and check icon

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -7,11 +7,11 @@ const Todo = (props: any) => {
         <div className='todo' draggable={true}>
             <div className={`todoContent${completed}`} onClick={props.todoClick} tabIndex={props.index}>
                 <div className='checkBubble' tabIndex={props.index}>
-                    <img className='check' src='./images/icon-check.svg' alt='' tabIndex={props.index} />
+                    <img className='check' src='./images/icon-check.svg' alt='complete' tabIndex={props.index} />
                 </div>
                 <p className='todoText' tabIndex={props.index}>{props.text}</p>
             </div>
-            <img className='remove' src="./images/icon-cross.svg" alt='' onClick={props.removeClick} tabIndex={props.index}/>
+            <img className='remove' src="./images/icon-cross.svg" alt='remove' onClick={props.removeClick} tabIndex={props.index}/>
         </div>
     )
 }
